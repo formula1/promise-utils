86748594eaf6fe4e883f78d5a9d9c9fc86049281
Samuel Tobia <sam@system76-pc.localdomain>
2021-01-23 15:39:25 -0800

    Cleanup: making functions into class methods

--- src/queue.ts
+++ src/queue.ts
@@ -10,38 +10,39 @@ class Queue {
   line: Array<[
     runnableFn, resolveFn, rejectFn
   ]> = [];
-  running = false;
+  running: boolean = false;
   run(fn: runnableFn){
-    const runNext = ()=>{
-      if(this.line.length){
-        var fnArgs = this.line.shift();
-        runFn(fnArgs[0], fnArgs[1], fnArgs[2])
-      } else {
-        this.running = false
-      }
-    }
-    const runFn = (
-      fn: runnableFn, res: resolveFn, rej: rejectFn
-    )=>{
-      return Promise.resolve().then(()=>{
-        return fn()
-      }).then((v)=>{
-        res(v)
-        runNext()
-      }, (e)=>{
-        rej(e);
-        runNext()
-      })
-    }
-
     return new Promise((res, rej)=>{
       if(this.running){
         this.line.push([fn, res, rej]);
       }else{
         this.running = true
-        runFn(fn, res, rej);
+        this.runFn(fn, res, rej);
       }
     });
+  }
+
+  private runNext(){
+    if(this.line.length){
+      var fnArgs = this.line.shift();
+      this.runFn(fnArgs[0], fnArgs[1], fnArgs[2])
+    } else {
+      this.running = false
+    }
+  }
+
+  private runFn(
+    fn: runnableFn, res: resolveFn, rej: rejectFn
+  ){
+    return Promise.resolve().then(()=>{
+      return fn()
+    }).then((v)=>{
+      this.runNext()
+      res(v)
+    }, (e)=>{
+      this.runNext()
+      rej(e);
+    })
   }
 }
 
